cbf785c1278033d2a285dd7a81194d26c3cc4691
Yaël Tramier <tramier.yael@gmail.com>
2022-11-16 14:54:40 +0000

    Rename project

---
+++ BikingServer/BikingServer/BikingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BikingServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class BikingService : IBikingService
+    {
+        public void CalculatePath(double startLongitude, double startLatitude, double endLongitude, double endLatitude)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BikingServer/BikingServer/IBikingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BikingServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IBikingService
+    {
+
+        // TODO: Add your service operations here
+        void CalculatePath(double startLongitude, double startLatitude, double endLongitude, double endLatitude);
+
+
+    }
+
+}
