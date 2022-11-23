b866e45f9489daf3b7390f6abe39c345bd566b70
JeremyGraffan <jeremy.graffan@etu.unice.fr>
2022-11-23 09:53:20 +0100

    [DEV] Introduce java client

---
+++ BikingClient/src/main/java/com/soc/testwsclient/TestWSclient.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.soc.testwsclient;
+
+/**
+ *
+ * @author franc
+ */
+public class TestWSclient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! we are going to test a SOAP client written in Java");
+        System.out.println("Resultat du subtract = ");
+    }
+}
