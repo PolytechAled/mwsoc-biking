14b19f03c6aea2c7132804fffc1d9be8092dbd0e
JeremyGraffan <jeremy.graffan@etu.unice.fr>
2022-11-30 14:07:54 +0100

    [DEV] Init generic cache

---
+++ BikingServer/BikingServerCache/Cache/GenericProxyCache.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikingServerCache.Models
+{
+    public class GenericProxyCache<T> : MemoryCache
+    {
+
+        public GenericProxyCache() : base(nameof(T))
+        {
+
+        }
+
+        public T Get(string CacheItemName)
+        {
+            if (Contains(CacheItemName))
+            {
+                return Get(CacheItemName);
+            }
+            return createCacheEntry(CacheItemName, InfiniteAbsoluteExpiration);
+           
+        }
+
+        public T Get(string CacheItemName, double dt_seconds)
+        {
+            if (!Contains(CacheItemName))
+            {
+                return createCacheEntry(CacheItemName, DateTimeOffset.Now.AddSeconds(dt_seconds));
+            }
+            return Get(CacheItemName);
+        }
+
+        public T Get(string CacheItemName, DateTimeOffset dt)
+        {
+
+            if (!Contains(CacheItemName))
+            {
+                return createCacheEntry(CacheItemName, dt);
+            }
+            return Get(CacheItemName);
+        }
+
+        private T createCacheEntry(string CacheItemName, DateTimeOffset expirationTime)
+        {
+            T obj = default;
+            Set(CacheItemName, obj, expirationTime);
+            return obj;
+        }
+    }
+}
