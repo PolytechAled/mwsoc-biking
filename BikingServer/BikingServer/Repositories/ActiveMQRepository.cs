bec4c901fdd640cedac8c37cab0ca53adc4a1337
JeremyGraffan <jeremy.graffan@etu.unice.fr>
2022-12-05 00:25:13 +0100

    [FIX] Message size after migration

--- BikingServer/BikingServer/Repositories/ActiveMQRepository.cs
+++ BikingServer/BikingServer/Repositories/ActiveMQRepository.cs
@@ -14,37 +14,22 @@ namespace BikingServer.Repositories
 {
     public class ActiveMQRepository
     {
-        private Uri uri;
-        private string queue;
         private ISession session;
         private IMessageProducer messageProducer;
 
-        public ActiveMQRepository(string uri = "activemq:tcp://localhost:61616", string queue = "BikingSteps")
+        public ActiveMQRepository(string uri = "activemq:tcp://localhost:61616")
         {
-            this.uri = new Uri(uri);
-            this.queue = queue;
-
-            ConnectionFactory connectionFactory = new ConnectionFactory(this.uri);
-
-            // Create a single Connection from the Connection Factory.
+            ConnectionFactory connectionFactory = new ConnectionFactory(uri);
             IConnection connection = connectionFactory.CreateConnection();
             connection.Start();
-
-            // Create a session from the Connection.
             session = connection.CreateSession();
-
-            // Use the session to target a queue.
-            IDestination destination = session.GetQueue("test");
-
-            // Create a Producer targetting the selected queue.
-            messageProducer = session.CreateProducer(destination);
-
-            // You may configure everything to your needs, for instance:
-            messageProducer.DeliveryMode = MsgDeliveryMode.NonPersistent;
         }
 
-        public void SendMessageInQueue(string message)
+        public void SendMessageInQueue(string queueName, string message)
         {
+            IDestination destination = session.GetQueue(queueName);
+            messageProducer = session.CreateProducer(destination);
+            messageProducer.DeliveryMode = MsgDeliveryMode.NonPersistent;
             ITextMessage msg = session.CreateTextMessage(message);
             messageProducer?.Send(msg);
         }
